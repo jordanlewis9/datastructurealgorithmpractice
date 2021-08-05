054047281b83b0f68e552c450c4551897e3c3ca7
Jordan Lewis <jlewis2008@live.com>
2021-08-04 20:57:05 -0400

    problem 5 w/ correct solution explanation by me

--- index.js
+++ index.js
@@ -21,24 +21,63 @@
 
 // console.log(averagePair([-4, -2, 0, 1, 2, 6, 8, 9], 3));
 
-const isSubsequence = (str1, str2) => {
-    if (str1.length === 0 || str2.length === 0) return false;
-    let str1pointer = 0;
-    let str2pointer = 0;
-    let isSame = false;
-    while (str1pointer < str1.length && str2pointer < str2.length) {
-        // console.log(str2pointer);
-        if (str1[str1pointer] === str2[str2pointer] && str1pointer === str1.length - 1) {
-            isSame = true;
-            break;
-        } else if (str1[str1pointer] === str2[str2pointer]) {
-            str1pointer++;
-            str2pointer++;
-        } else {
-            str2pointer++;
-        }
+// const isSubsequence = (str1, str2) => {
+//     if (str1.length === 0 || str2.length === 0) return false;
+//     let str1pointer = 0;
+//     let str2pointer = 0;
+//     let isSame = false;
+//     while (str1pointer < str1.length && str2pointer < str2.length) {
+//         // console.log(str2pointer);
+//         if (str1[str1pointer] === str2[str2pointer] && str1pointer === str1.length - 1) {
+//             isSame = true;
+//             break;
+//         } else if (str1[str1pointer] === str2[str2pointer]) {
+//             str1pointer++;
+//             str2pointer++;
+//         } else {
+//             str2pointer++;
+//         }
+//     }
+//     return isSame;
+// }
+
+// console.log(isSubsequence('abc', 'acb'));
+
+// const maxSubArraySum = (arr, inARow) => {
+//     if (arr.length < inARow) return null;
+//     let leftWindow = 0;
+//     let rightWindow = 0 + (inARow - 1);
+//     let max;
+//     while (rightWindow < arr.length) {
+//         let tempMax = arr.slice(leftWindow, rightWindow + 1).reduce((acc, cur) => {
+//             return acc + cur;
+//         }, 0);
+//         if (leftWindow === 0 || tempMax > max) {
+//             max = tempMax;
+//         }
+//         leftWindow++;
+//         rightWindow++;
+//     }
+//     return max;
+// }
+
+// console.log(maxSubArraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 10));
+
+function maxSubarraySum(arr, num){
+    if (arr.length < num) return null;
+ 
+    let total = 0;
+    for (let i=0; i<num; i++){
+       total += arr[i];
     }
-    return isSame;
+    let currentTotal = total;
+    for (let i = num; i < arr.length; i++) {
+       currentTotal += arr[i] - arr[i-num];
+       total = Math.max(total, currentTotal);
+    }
+    return total;
 }
 
-console.log(isSubsequence('abc', 'acb'));
+// with currentTotal, we are adding the new number, and subtracting the array index that we are not on anymore
+
+console.log(maxSubarraySum([100, 200, 300, 400], 2));
