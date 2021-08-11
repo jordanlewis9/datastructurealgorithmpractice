007a7ce3ba1405edcab6a31f5daa5b465dc3aa70
Jordan Lewis <jlewis2008@live.com>
2021-08-10 21:30:13 -0400

    sliding window problem

--- index.js
+++ index.js
@@ -63,21 +63,55 @@
 
 // console.log(maxSubArraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 10));
 
-function maxSubarraySum(arr, num){
-    if (arr.length < num) return null;
+// function maxSubarraySum(arr, num){
+//     if (arr.length < num) return null;
  
-    let total = 0;
-    for (let i=0; i<num; i++){
-       total += arr[i];
-    }
-    let currentTotal = total;
-    for (let i = num; i < arr.length; i++) {
-       currentTotal += arr[i] - arr[i-num];
-       total = Math.max(total, currentTotal);
-    }
-    return total;
-}
+//     let total = 0;
+//     for (let i=0; i<num; i++){
+//        total += arr[i];
+//     }
+//     let currentTotal = total;
+//     for (let i = num; i < arr.length; i++) {
+//        currentTotal += arr[i] - arr[i-num];
+//        total = Math.max(total, currentTotal);
+//     }
+//     return total;
+// }
 
 // with currentTotal, we are adding the new number, and subtracting the array index that we are not on anymore
 
-console.log(maxSubarraySum([100, 200, 300, 400], 2));
+// console.log(maxSubarraySum([100, 200, 300, 400], 2));
+
+const minSubArrayLen = (arr, total) => {
+    if (arr.length === 1 && arr[0] >= total) {
+        return 1;
+    } else if (arr.length === 1 && arr[0] < total) {
+        return 0;
+    } else if (arr[0] >= total) {
+        return 1;
+    }
+    let leftSide = 0;
+    let rightSide = 1;
+    let subarrayLength = 0;
+    while (rightSide < arr.length) {
+        let arrTotal = 0;
+        for (let i = leftSide; i <= rightSide; i++) {
+            arrTotal += arr[i];
+        }
+        if (arrTotal >= total) {
+            let totalLength = rightSide - leftSide + 1;
+            if (totalLength === 1) {
+                subarrayLength = totalLength;
+                break;
+            } else if (subarrayLength === 0 || totalLength < subarrayLength) {
+                subarrayLength = totalLength;
+            }
+            leftSide++;
+        } else {
+            rightSide++;
+        }
+    }
+    return subarrayLength;
+}
+
+console.log(minSubArrayLen([1, 2, 4, 6, 5], 4));
